20dcb3da6fc32a87fb51debf92feb367d2c3dfab
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-03-25 17:42:53 +0600

    page layout

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,10 +1,21 @@
 
 
 const Job = ({ job }) => {
-    const { logo } = job
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className="card card-compact bg-base-100 shadow-xl">
+            <figure><img src={logo} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">{remote_or_onsite}</button>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">{job_type}</button>
+                </div>
+                <div className="card-actions">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };
